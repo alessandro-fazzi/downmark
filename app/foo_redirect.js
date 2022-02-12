736c926671c28efed93edfc8a11633039b9b3b68
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2022-02-12 22:28:44 +0100

    Cleanup a bit the mess and add bookmarklet build script

--- app/foo_redirect.js
+++
@@ -1,3 +0,0 @@
-javascript: (() => {
-  document.location.href = 'https://downmark.herokuapp.com/obsidian?u=' + encodeURIComponent(document.location) + '&platform=' + window.navigator.platform
-})()
---
+++ bookmarklet/bookmarklet_src.js
@@ -0,0 +1,1 @@
+document.location.href = 'https://downmark.herokuapp.com/obsidian?u=' + encodeURIComponent(document.location) + '&platform=' + window.navigator.platform
